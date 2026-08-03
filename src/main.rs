f9aa8d4bb4d8782ce81f8f12ed3ed1575c15537c
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-08-03 18:54:40 +0100

    source: source code reading
    Stub with some structures and documentation.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod interner;
+mod source;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/source.rs
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Structures                                                                 *
+ ******************************************************************************/
+/// A byte span, composed of a start and end position.
+/// NOTE: This span maps to [start, end) i.e. an exclusive ended range.
+#[derive(Debug, Eq, PartialEq, Hash, Copy, Clone, Default)]
+pub struct Span {
+    pub start: u32,
+    pub end: u32,
+}
+
+/// Character Line-Column position in some source text.
+/// NOTE: Default initialisation sets these to {1, 1}.
+/// NOTE: These are not byte-oriented, but character oriented.
+#[derive(Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Copy, Clone)]
+pub struct Position {
+    pub line: usize,
+    pub col: usize,
+}
+
+/// Source text for scanning purposes.
+pub struct Source {
+    name: String,
+    contents: String,
+    line_starts: Vec<u32>,
+}
+

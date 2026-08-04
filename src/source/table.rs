915c02c6d18daf8ae6c2f51db5109728e8897437
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-08-05 00:37:39 +0100

    source:table: Fix documentation for `SourceTable::location_of`

--- src/source/table.rs
+++ src/source/table.rs
@@ -128,7 +128,7 @@ impl SourceTable {
         &self.origins[id]
     }
 
-    /// Get the [Location] within the source of a a given `id`
+    /// Get the [Location] within the source of a given `id`
     #[must_use]
     pub fn location_of(&self, id: SyntaxId) -> Location<'_> {
         let SyntaxOrigin { source, span } = self.origins[id.0 as usize];

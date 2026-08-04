886584a3e6c8cfd902e994c5f37703cdc5cbbf13
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-08-04 03:20:53 +0100

    source:table: sketch out basic SourceTable data
    This is Session-scoped, and will outlive the HIR phase.  I've sketched
    out the basic data types now.
    
    SourceTable is really the big object that comes out of the
    SourceModule, and will be directly consumed by all other phases.  So
    it'll be eating up a bunch of the stuff that currently Source does.

--- src/source.rs
+++ src/source.rs
@@ -1,5 +1,7 @@
 mod span;
+mod table;
 mod text;
 
 pub use span::{Position, Span};
+pub use table::{Location, SourceId, SourceTable, SyntaxId, SyntaxOrigin};
 pub use text::{MAX_SOURCE_LEN, Source, SourceError};
---
+++ src/source/table.rs
@@ -0,0 +1,88 @@
+use crate::source::{Position, Source, SourceError, Span};
+
+/******************************************************************************
+ * Structures                                                                 *
+ ******************************************************************************/
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub struct SourceId(u32);
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub struct SyntaxId(u32);
+
+#[derive(Debug, Copy, Clone)]
+pub struct SyntaxOrigin {
+    pub source: SourceId,
+    pub span: Span,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct Location<'a> {
+    pub file: &'a str,
+    pub start: Position,
+    pub end: Position,
+}
+
+#[derive(Debug)]
+pub struct SourceTable {
+    sources: Vec<Source>,
+    origins: Vec<SyntaxOrigin>,
+}
+
+#[derive(Debug)]
+pub enum SourceTableError {
+    SourceCreate(SourceError),
+    Io(std::io::Error),
+}
+
+impl SourceTable {
+    pub fn new() -> Self {
+        Self {
+            sources: Vec::new(),
+            origins: Vec::new(),
+        }
+    }
+
+    pub fn add_source_file(
+        &mut self,
+        file_name: &str,
+    ) -> Result<SourceId, SourceTableError> {
+        let contents = std::fs::read_to_string(file_name)?;
+        self.add_source_raw(file_name, contents)
+    }
+
+    pub fn add_source_raw(
+        &mut self,
+        source_name: &str,
+        contents: String,
+    ) -> Result<SourceId, SourceTableError> {
+        let source = Source::from_contents(source_name, contents)?;
+        let id = SourceId(self.sources.len() as u32);
+        self.sources.push(source);
+        Ok(id)
+    }
+}
+
+impl From<SourceError> for SourceTableError {
+    fn from(e: SourceError) -> Self {
+        Self::SourceCreate(e)
+    }
+}
+
+impl From<std::io::Error> for SourceTableError {
+    fn from(e: std::io::Error) -> Self {
+        Self::Io(e)
+    }
+}
+
+impl std::fmt::Display for SourceTableError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SourceTableError::SourceCreate(e) => {
+                write!(f, "{e}")
+            }
+            SourceTableError::Io(e) => {
+                write!(f, "{e}")
+            }
+        }
+    }
+}

81528e2f7b45a5b82caa964ef37ac89c31625f98
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-08-05 07:03:44 +0100

    diagnostics: introduce accumulator
    For the fallible phases of the compiler, they'll usually want to
    accumulate diagnostics rather than fail at just the first one.  This
    builder handles that, and provides accessors for reporting.

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -11,4 +11,7 @@ pub use class::{Class, Severity};
 mod diagnostic;
 pub use diagnostic::{Diagnostic, Site};
 
+mod accumulator;
+pub use accumulator::{DEFAULT_DIAGNOSTIC_CAP, Diagnostics};
+
 pub mod conv;

9b3ab31ab7bf1df18a57adc2110cb40fdc9fdfad
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-08-10 07:26:25 +0100

    main: basic "cat-like" behaviour program
    Wiring up the diagnostics system and the SourceTable.  When we finish
    the lexer + testing, we can stick that onto here.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,67 @@
 //! Entrypoint for rForsp compiler
 
-fn main() {
-    println!("Hello, world!");
+use std::{io::Write, process::ExitCode};
+
+use rforsp::{
+    diagnostics::{Diagnostics, render_diagnostics},
+    source::SourceTable,
+};
+
+/// Print usage to the given [`Write`] target.
+fn usage(mut out: impl std::io::Write) {
+    let _ = writeln!(
+        out,
+        concat!(
+            "Usage: rforsp [FILES]\n",
+            "Compile the given FILES sequentially as rforsp source code."
+        )
+    );
+}
+
+/// Report errors from [`Diagnostics`] to [`std::io::stderr`] if there are any.
+fn report_errors(table: &SourceTable, diagnostics: &Diagnostics) -> bool {
+    if diagnostics.has_errors() {
+        let mut error_buf = String::new();
+        let _ = render_diagnostics(diagnostics, table, &mut error_buf);
+        let _ = std::io::stderr().write_all(error_buf.as_bytes());
+        true
+    } else {
+        false
+    }
+}
+
+fn main() -> ExitCode {
+    if std::env::args().len() == 1 {
+        usage(std::io::stderr());
+        return ExitCode::FAILURE;
+    }
+
+    let mut diagnostics = Diagnostics::new();
+    let mut table = SourceTable::new();
+    let sources = std::env::args()
+        .skip(1)
+        .map_while(|filename| {
+            table
+                .add_source_file(&filename)
+                .map_err(|e| {
+                    diagnostics.push(e.into());
+                })
+                .ok()
+        })
+        .collect::<Vec<_>>();
+
+    if diagnostics.has_errors() {
+        report_errors(&table, &diagnostics);
+        ExitCode::FAILURE
+    } else {
+        for source in sources.iter().map(|&id| table.get_source(id)) {
+            println!(
+                concat!("SOURCE[{}]:\n", "<start>\n", "{}", "<end>\n"),
+                source.name,
+                source.text()
+            );
+        }
+
+        ExitCode::SUCCESS
+    }
 }

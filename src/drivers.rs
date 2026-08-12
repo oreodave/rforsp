060aaaf43f3287acbe9be4415f34e22d76e92853
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-08-12 11:50:18 +0100

    drivers: add `tokenise_sources` driver

--- src/drivers.rs
+++ src/drivers.rs
@@ -2,14 +2,30 @@
 
 use crate::{
     diagnostics::{Aborted, Diagnostics, Phase},
+    lexer::{Token, tokenise},
     source::{SourceId, SourceTable},
 };
+
+/// The gate that ensures that the results of a compiler phase only pass through
+/// if the [`Diagnostics`] of that phase have no errors.
+fn gate<T>(
+    global_diags: &mut Diagnostics,
+    local_diags: Diagnostics,
+    container: T,
+    phase: Phase,
+) -> Result<T, Aborted> {
+    let succeeded = !local_diags.has_errors();
+    global_diags.merge(local_diags);
+    succeeded
+        .then_some(container)
+        .ok_or_else(|| Aborted::new(phase))
+}
 
 /// Add a set of files to the given [`SourceTable`].
 ///
 /// # Errors
-/// - If any [`Diagnostic`][crate::diagnostics::Diagnostic]s are created while
-///   adding files to the table.
+/// - If any error [`Diagnostic`][crate::diagnostics::Diagnostic]s are created
+///   while adding files to the table.
 pub fn sources_from_files(
     filenames: &[String],
     source_table: &mut SourceTable,
@@ -26,10 +42,28 @@ pub fn sources_from_files(
         })
         .collect::<Vec<_>>();
 
-    let succeeded = !local.has_errors();
+    gate(diagnostics, local, sources, Phase::Source)
+}
 
-    diagnostics.merge(local);
-    succeeded
-        .then_some(sources)
-        .ok_or(Aborted::new(Phase::Source))
+/// Lex a sequence of [`SourceId`] into Token Streams.
+///
+/// # Errors
+/// - If any error [`Diagnostic`][crate::diagnostics::Diagnostic]s are created
+///   while lexing the given sources.
+pub fn lex_sources(
+    source_ids: &[SourceId],
+    source_table: &SourceTable,
+    diagnostics: &mut Diagnostics,
+) -> Result<Vec<Vec<Token>>, Aborted> {
+    let mut local = Diagnostics::new();
+    let tokens_set = source_ids
+        .iter()
+        .filter_map(|&id| {
+            let (tokens, local_1) = tokenise(id, source_table);
+            local.merge(local_1);
+            tokens
+        })
+        .collect::<Vec<_>>();
+
+    gate(diagnostics, local, tokens_set, Phase::Lex)
 }

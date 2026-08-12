5e4c7172e4ae5d28fb5795b4a87d410a4955a904
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-08-12 16:35:12 +0100

    main: CLI arg parsing, including log setting.

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,102 @@ use std::process::ExitCode;
 use rforsp::{
     context::Compilation,
     diagnostics::{Diagnostics, render_diagnostics},
-    drivers::compile,
+    drivers::{Log, compile},
 };
+
+/// Configuration for CLI driver.
+struct CliConfig {
+    /// Log level.
+    log: Log,
+    /// Files to compile.
+    files: Vec<String>,
+}
+
+/// Types of immediate CLI exit following command line parsing.
+enum CliExit {
+    /// Immediately exit with failure.
+    Failure,
+    /// Immediately exit with success.
+    Success,
+}
+
+/// Parse command line arguments
+fn parse_cli() -> Result<CliConfig, CliExit> {
+    let mut config = CliConfig {
+        log: Log::None,
+        files: Vec::new(),
+    };
+    let mut exit = None;
+    let mut args = std::env::args().skip(1).peekable();
+
+    while args.peek().is_some_and(|arg| arg.starts_with("--")) {
+        match args.next().unwrap_or_default().as_str() {
+            "--log-tokens" => config.log = Log::Tokens,
+            "--help" => {
+                usage(std::io::stdout());
+                exit = Some(CliExit::Success);
+                break;
+            }
+            "--version" => {
+                println!("rforsp v0.0.0");
+                exit = Some(CliExit::Success);
+                break;
+            }
+            unknown => {
+                eprintln!("Unknown argument `{unknown}`.");
+                exit = Some(CliExit::Failure);
+                break;
+            }
+        }
+    }
+
+    if let Some(exit) = exit {
+        Err(exit)
+    } else if args.len() == 0 {
+        Err(CliExit::Failure)
+    } else {
+        config.files = args.collect();
+        Ok(config)
+    }
+}
 
 /// Print usage to the given [`Write`] target.
 fn usage(mut out: impl std::io::Write) {
-    let _ = writeln!(
+    let _ = write!(
         out,
         concat!(
-            "Usage: rforsp [FILES]\n",
-            "Compile the given FILES sequentially as rforsp source code."
+            "Usage: rforsp [OPTIONS] [FILES]\n",
+            "Compile the given FILES sequentially as rforsp source code.\n",
+            "Options:\n",
+            "  --help:       Print this help and exit.\n",
+            "  --version:    Print version of program.\n",
+            "  --log-tokens: Print tokens generated per FILE.\n"
         )
     );
 }
 
 fn main() -> ExitCode {
-    let args = std::env::args().skip(1).collect::<Vec<_>>();
-    if args.is_empty() {
-        usage(std::io::stderr());
-        return ExitCode::FAILURE;
-    }
+    let config = match parse_cli() {
+        Err(CliExit::Failure) => {
+            usage(std::io::stderr());
+            return ExitCode::FAILURE;
+        }
+        Err(CliExit::Success) => {
+            return ExitCode::SUCCESS;
+        }
+        Ok(cfg) => cfg,
+    };
+
+    let args = config.files;
 
     let mut ctx = Compilation::new();
     let mut diagnostics = Diagnostics::new();
 
-    let compile_result = compile(&args, &mut ctx, &mut diagnostics);
+    let mut log_buf = String::new();
+    let compile_result =
+        compile(&args, &mut ctx, &mut diagnostics, config.log, &mut log_buf);
+
+    print!("{log_buf}");
 
     match compile_result {
         Err(e) => {

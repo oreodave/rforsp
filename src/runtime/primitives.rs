28b22f7474b7193c387463d91772fbf35e233533
Aryadev Chavali <aryadev@aryadevchavali.com>
2026-08-17 08:41:11 +0100

    runtime:primitives: methods for `PrimitiveRegistry`, including default init.

--- src/runtime/primitives.rs
+++ src/runtime/primitives.rs
@@ -1,15 +1,15 @@
 //! Primitive type and registry.
 
-use crate::interner::SymId;
+use crate::interner::{Interner, SymId};
 
 /// ID for an entry in the primitive registry.
-pub struct PrimitiveId(u32);
+pub struct PrimitiveId(usize);
 
 /// Type for primitive
 pub struct Primitive {
     /// Symbol attached to this primitive.
     sym: SymId,
-    // FIXME(oreo)[2026-08-17 08:26]:: fields for stack signature, heap effect,
+    // FIXME(oreo)[2026-08-17 08:26]: fields for stack signature, heap effect,
     // runtime impl, and lowering.
 }
 
@@ -17,4 +17,50 @@ pub struct Primitive {
 pub struct PrimitiveRegistry {
     /// Raw vector of primitives from which [`PrimitiveId`] is derived.
     primitives: Vec<Primitive>,
+}
+
+impl PrimitiveRegistry {
+    /// Names of default primitives within the rForsp language.
+    /// NOTE: ordering is important for the [`Self::with_builtins`] method.
+    const PRIMITIVE_NAMES: [&'static str; 26] = [
+        "cons", "car", "cdr", "eq", "cswap", "tag", "read", "write", "+", "-",
+        "*", "/", "&", "|", "~&", "<<", ">>", "rec", "if", "copy", "length",
+        "vmake", "vpush", "vpop", "vget", "vset",
+    ];
+
+    /// Construct a new [`PrimitiveRegistry`].
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {
+            primitives: Vec::new(),
+        }
+    }
+
+    /// Construct a new [`PrimitiveRegistry`] pre-initialised with expected
+    /// primitives.
+    pub fn with_builtins(interner: &mut Interner) -> Self {
+        let mut registry = Self::new();
+        for name in Self::PRIMITIVE_NAMES {
+            let sym = interner.intern(name);
+            let _ = registry.add(sym);
+        }
+        registry
+    }
+
+    /// Add a primitive to the registry, returning a [`PrimitiveId`] for the new
+    /// addition.
+    #[must_use]
+    pub fn add(&mut self, sym: SymId) -> PrimitiveId {
+        // FIXME(oreo)[2026-08-17 08:31]: propagate fields required for
+        // `Primitive` to function signature.
+        let id = PrimitiveId(self.primitives.len());
+        self.primitives.push(Primitive { sym });
+        id
+    }
+}
+
+impl Default for PrimitiveRegistry {
+    fn default() -> Self {
+        Self::new()
+    }
 }
